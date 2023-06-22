6a2ef58f39674b27b8feb6d415f66073705cc605
Stu Hemerling <72936491+swayneh@users.noreply.github.com>
2023-06-22 16:59:08 -0700

    first commit

--- run_first.R
+++ run_first.R
@@ -6,5 +6,5 @@ data_01 <- read_excel(excel_file, sheet = "0.1", range = "a5:c8", col_names = TR
 data_02 <- read_excel(excel_file, sheet = "0.2", range = "a3:e6", col_names = TRUE)
 data_03 <- read_excel(excel_file, sheet = "0.3", range = "a2:e12", col_names = TRUE)
 
-
+ghgfhf
 data_01
