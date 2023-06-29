df121b3adb925c097726922e270ccd727a345267
Stu Hemerling <72936491+swayneh@users.noreply.github.com>
2023-06-29 15:00:18 -0700

    Added stacked area chart

--- app/app.R
+++ app/app.R
@@ -6,16 +6,40 @@ ui <- function(req) {
 
 
     ## Replace appname with the title that will appear in the header
-    bcsapps::bcsHeaderUI(id = 'header', appname = "Replace with app title"),
+    bcsapps::bcsHeaderUI(id = 'header', appname = "Small Business in BC"),
 
     column(width = 12,
            style = "margin-top:100px",
 
-           "Add elements to include in the app here"
-           ),
+           "xxx"
+           )
+  ),
+
+
+
+  fluidRow(
+    box(title = "Pie Chart", plotlyOutput("pie_chart"), width = 6),
+    box(title = "Bar Chart", plotlyOutput("bar_chart"), width = 6)
+  ),
+
+  fluidRow(
+    box(title = "Horizontal Bar Chart", plotlyOutput("hbar_chart"), width = 6),
+    box(title = "Horizontal Bar Chart (Add to 100%)", plotlyOutput("hbar_chart_100"), width = 6)
+  ),
+  fluidRow(
+    box(title = "Line Chart", plotlyOutput("line_chart"), width = 6),
+    box(title = "Area Chart", plotlyOutput("area_chart"), width = 6)
+  ),
+  fluidRow(
+    box(title = "Static Table", gt_output("my_table"))
+  ),
+
+  fluidRow(
+
 
     bcsapps::bcsFooterUI(id = 'footer')
   )
+
 )}
 
 server <- function(input, output) {
@@ -25,6 +49,80 @@ server <- function(input, output) {
 
   bcsapps::bcsFooterServer(id = 'footer')
 
-  }
+  category = data_01$`Type of Business`
+  percentage = data_01$Percentage
+
+  output$pie_chart <- renderPlotly({
+    pie_chart <- plot_ly(data_01, labels = ~category, values = ~percentage,
+                         textinfo = "label+percent", showlegend = FALSE,
+                         textposition = "outside", insidetextfont = list(color= "#FFFFFF"), type = "pie")
+    pie_chart
+  })
+
+  output$bar_chart <- renderPlotly({
+    bar_chart <- plot_ly(data_02, x = ~Category, y = ~Value, type = "bar")
+    bar_chart
+  })
+
+  output$hbar_chart <- renderPlotly({
+    hbar_chart <- plot_ly(data_03, x = ~Value, y = ~Category, type = "bar", orientation = "h")
+    hbar_chart
+  })
+
+  output$hbar_chart_100 <- renderPlotly({
+    hbar_chart_100 <- plot_ly(data_04, x = ~Value, y = ~Category, type = "bar", orientation = "h") %>%
+      layout(barmode = "stack")
+    hbar_chart_100
+  })
+
+  output$line_chart <- renderPlotly({
+    line_chart <- plot_ly(data_05, x = ~Category, y = ~Value, type = "scatter", mode = "lines")
+    line_chart
+  })
+
+
+
+  output$area_chart <- renderPlotly({
+
+    area_chart <- plot_ly(data_04_result, x = ~years, y = data_04_result$`Small business exports`, type = "scatter", mode = "lines", fillcoler = "#455555" , name = "Small B", stackgroup = 'one') %>%
+      add_trace(y = data_04_result$`Large business exports`, type = "scatter", mode = "lines", fillcolor = "tozeroy", name = "Large B") %>%
+
+      layout(title = " Comparison",
+             xaxis = list(title = "Year"),
+             yaxis = list(title = "Billions"),
+             legend = list(title = "Type of Business"))
+
+
+    area_chart
+  })
+
+
+  # Render the table
+  output$my_table <- render_gt({
+
+
+  # Create the gt table
+  tbl <- gt(data_02)
+
+  # Add a header row with blue background and white text
+  tbl <- tbl |>
+    tab_header(
+      title = md("**Figure3.1 Number of self-employed business owners in British Columbia, 2021**"),
+
+    )
+  tbl <- tbl |>
+    tab_source_note(
+      source_note = "Source: This is where the data comes from"
+
+    )
+
+
+
+
+    return(tbl)
+  })
+}
+
+
 
 shiny::shinyApp(ui = ui, server = server)
--- run_first.R
+++ run_first.R
@@ -1,12 +1,34 @@
 library(readxl)
-library(ggplot2)
+library(gt)
+library(tidyverse)
 
 # Read the Excel file
 excel_file <- "C:/bc-small-business/app/data/SBP2023_Chart_data2.xlsx"
 data_01 <- read_excel(excel_file, sheet = "0.1", range = "a5:c8", col_names = TRUE)
+data_01
+
 data_02 <- read_excel(excel_file, sheet = "0.2", range = "a3:e6", col_names = TRUE)
 data_03 <- read_excel(excel_file, sheet = "0.3", range = "a2:e12", col_names = TRUE)
-data_04 <- read_excel(excel_file, sheet = "0.4", range = "a3:n5", col_names = TRUE)
+
+data_04 <- read_excel(excel_file, sheet = "0.4", range = "a3:n5", col_names = FALSE)
+data_04t <- t(data_04)
+data_04_result <- as.data.frame(data_04t)
+colnames(data_04_result) <- data_04_result[1, ]
+data_04_result <- data_04_result[-1, ]
+data_04_result
+
+
+data_04_transformed <-data_04 %>%
+  pivot_longer(-business, names_to = "years", values_to = "value")
+ # pivot_wider(names_from = "year", values_from = "value")
+
+data_04_transformed
+
+print(data_04_long)
+
+data_04a <- read_excel(excel_file, sheet = "0.4", range = "a3:n5", col_names = TRUE)
+print(data_04a)
+
 data_05 <- read_excel(excel_file, sheet = "Key1", range = "a3:b12", col_names = FALSE)
 data_06 <- read_excel(excel_file, sheet = "Key2", range = "a5:c15", col_names = TRUE)
 data_07 <- read_excel(excel_file, sheet = "Key2", range = "a23:b33", col_names = FALSE)
