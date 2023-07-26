8a2ab50f8a68015a5c238a2b5c79010fe6915c1d
stephanieyurchak <Stephanie.Yurchak@gov.bc.ca>
2023-07-26 14:06:50 -0700

    function to add horizontal lines to charts

--- app/R/functions.R
+++ app/R/functions.R
@@ -1,2 +1,14 @@
 
 ## Use this file to save functions that your app will use
+
+hline <- function(y = 0, color = "black") {
+  list(
+    type = "line",
+    x0 = 0,
+    x1 = 1,
+    xref = "paper",
+    y0 = y,
+    y1 = y,
+    line = list(color = color)
+  )
+}
